ca4200b7c383e0cf0286da208f58d1dd4a81c4f0
dmedeveloper <89945043+dmedeveloper@users.noreply.github.com>
2021-10-14 16:41:58 +0530

    Update vuePdfNoSss.vue

--- src/vuePdfNoSss.vue
+++ src/vuePdfNoSss.vue
@@ -1,15 +1,16 @@
 <style src="./annotationLayer.css"></style>
 <script>
     import componentFactory from './componentFactory.js'
+    import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
 
     if (process.env.VUE_ENV !== 'server') {
         var pdfjsWrapper = require('./pdfjsWrapper.js').default;
-        var PDFJS = require('pdfjs-dist/webpack');
+        var PDFJS = require("pdfjs-dist");
 
         if (typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1) {
             var PdfjsWorker = require('worker-loader!pdfjs-dist/build/pdf.worker.js');
 
-            PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
+            PDFJS.GlobalWorkerOptions.workerSrc = pdfjsWorker;
         }
 
         var component = componentFactory(pdfjsWrapper(PDFJS));
